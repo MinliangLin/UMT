3613766ed4648ab14917eac36b65931c1d4acfb6
HaifangQin <haifang.qin@hotstar.com>
2023-09-18 12:15:03 +0000

    feat: add audio feature extraction

---
+++ code_reading_notes.md
@@ -0,0 +1,9 @@
+1. This code take fully usage of nncore.
+2. most config are using python native `dict`.
+3. datasets class will be loaded by `configs/_base_/datasets/qvhighlights.py`
+
+clip feature: (75, 512)
+clip sub:
+    last_hidden_state: (9, 512)
+    pooler_output: (512,)
+pann_feature: (75, 2048)
---
+++ select_audio.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+root = Path("/home/ubuntu/short_form/data/")
+
+
+def coalesce(content_id):
+    audio_list = ["en", "hin", "hi"]
+    for lang in audio_list:
+        path = root / f"{content_id}/{content_id}_audio_{lang}.mp4"
+        if path.is_file():
+            return path
+
+
+with open("select_audio.sh", "w") as f:
+    for p in root.glob("*/*.json"):
+        content_id = p.stem
+        audio = coalesce(content_id)
+        if not audio:
+            continue
+        cmd = f'ln -s {audio} {audio.parent / "audio.mp4"}\n'
+        f.write(cmd)
